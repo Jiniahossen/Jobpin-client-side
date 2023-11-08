c9e8e424fc409dc91a088abb37b0c7e54a61e65d
jinia hossen <jiniahossen31@gmail.com>
2023-11-08 18:54:21 +0600

    Blog page added

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
-.In All job page, I have fetched all the jobs data from database and display in a tabular form . I also impliment a search functionality , which will searched a job post by Job Title .
+.In All job page, I have fetched all the jobs data from database and display in a tabular form . I also impliment a search functionality , which will searched a job post by Job Title
 
-.All job page  display  jobs with a view details button ,after clicking the view button redirect a new route called /job/:id ,this route will display specicific job post details .
+.All job page  display  jobs with a view details button ,after clicking the view button redirect a new route called /job/:id ,this route will display specicific job post details
 
-.Job Details page have also a apply button if user want to apply for a job ,he/she can apply by clicking apply button ,if the user want to apply for their own job function will show a error showing  User can't aplly for their own job.
+.Job Details page have also a apply button if user want to apply for a job ,he/she can apply by clicking apply button ,if the user want to apply for their own job function will show a error showing  User can't aplly for their own job
 
-.Applied job pages will show the data the user have applied for job .this will show specific user data ,which I have get using email from database.
+.Applied job pages will show the data the user have applied for job .this will show specific user data ,which I have get using email from database
 
-.My job route will show the job details ,if user post job circullar this route will show all the job post in a tabular form.
+.My job route will show the job details ,if user post job circullar this route will show all the job post in a tabular form
--- src/Pages/Blogs.jsx
+++ src/Pages/Blogs.jsx
@@ -55,7 +55,7 @@ const Blogs = () => {
                         NestJS is a progressive, TypeScript-based web application framework built on top of Express.js. It is an opinionated framework, meaning that it provides a more structured and opinionated way of building applications. This can make it easier for developers to get started and to maintain consistent code across their applications.
 
                         NestJS uses a modular architecture, and it provides a number of features to help developers build scalable and maintainable applications, such as dependency injection, decorators, and modules.
-<br />
+                        <br />
                         Key features of NestJS:
                         <ul>
                             <li>TypeScript-based</li>
@@ -68,6 +68,19 @@ const Blogs = () => {
                     </p>
 
                 </div>
+            </div>
+
+            <div className=" text-lg font-serif mb-20">
+                <h1 className=" text-3xl mb-4">Website functionality:</h1>
+                <li>In All job page, I have fetched all the jobs data from database and display in a tabular form . I also impliment a search functionality , which will searched a job post by Job Title</li>
+                <li>
+                    All job page  display  jobs with a view details button ,after clicking the view button redirect a new route called /job/:id ,this route will display specicific job post details </li>
+
+                <li>Job Details page have also a apply button if user want to apply for a job ,he/she can apply by clicking apply button ,if the user want to apply for their own job function will show a error showing  User cant aplly for their own job</li>
+
+                <li> Applied job pages will show the data the user have applied for job .this will show specific user data ,which I have get using email from database</li>
+
+                <li>My job route will show the job details ,if user post job circullar this route will show all the job post in a tabular form</li>
             </div>
 
         </div>
